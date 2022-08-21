8a9bb2dedaeb3ccbc88612c6e927ca3aa8c92e8f
Evelyn Koo <r_evelynkoo@berkeley.edu>
2022-08-21 00:26:58 -0700

    add some labels

--- .config/nvim/luasnip/lua.lua
+++ .config/nvim/luasnip/lua.lua
@@ -6,14 +6,14 @@ return {
     { <> },
     { delimiters='<>' }
     )<>)<>]],
-    { i(1, "trig"), i(2, "trig"), i(3, "dscr"), i(4, "fmt"), i(5, "inputs"), i(6, "<>"), i(7), i(0)},
+    { i(1, "trig"), i(2, "trig"), i(3, "dscr"), i(4, "fmt"), i(5, "inputs"), i(6, "<>"), i(7, "opts"), i(0)},
     { delimiters='<>' }
     )),
     -- simple text snippet 
     s("snipt", fmt([[ 
     s('<>', {t('<>')}<>
     <>)<>]],
-    { i(1, "trig"), i(2, "text"), i(3), i(4), i(0)},
+    { i(1, "trig"), i(2, "text"), i(3, "opts"), i(4), i(0)},
     { delimiters='<>' }
     )),
 }, {
