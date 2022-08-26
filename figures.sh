8ed4888be05d96235a6bd81669892afb25ee4b9f
Evelyn Koo <r_evelynkoo@berkeley.edu>
2022-08-26 10:55:56 -0700

    scripts

--- figures.sh
+++ figures.sh
@@ -2,16 +2,15 @@
 
 # discor
 
-screenshot_dir="$HOME/Documents/university/fdir"
-figures_dir="$screenshot_dir/figures"
-fileout="$figures_dir/$(date +'%Y-%m-%d_%H-%M-%S')" # may change depending on me getting yaml or something to organize stuff
-
 # stuff 
 if [ "$1" == "-s" -o "$1" == "-setdir" ]; then
     new_dir=$(pwd)
-    unlink ~/Documents/university/fdir/
-    ln -s $new_dir ~/Documents/university/fdir
+    #unlink ~/Documents/university/fdir/
+    ln -sf $new_dir ~/Documents/university/fdir
 else
+    screenshot_dir="$HOME/Documents/university/fdir"
+    figures_dir="$screenshot_dir/figures"
+    fileout="$figures_dir/$(date +'%Y-%m-%d_%H-%M-%S')" # may change depending on me getting yaml or something to organize stuff
     [ -d "$figures_dir" ] || mkdir figures_dir
     [ -d "$figures_dir" ]; /usr/bin/gnome-screenshot -c -a -f "$fileout" $@
 fi
