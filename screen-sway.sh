943be9599c1b4f098cbc5031bb7d1fac9bc1dea2
Evelyn Koo <r_evelynkoo@berkeley.edu>
2022-09-16 20:31:34 -0700

    something

--- screen-sway.sh
+++ screen-sway.sh
@@ -3,5 +3,4 @@
 # couple lines of code to take screenshots and save with a file name.
 screenshot_dir="$HOME/Pictures/Screenshots/"
 fileout="$screenshot_dir/$(zenity --entry --title="Take a Screenshot" --text="Image Name:").png"
-[ -d "$screenshot_dir" ]; grim -g "$(slurp)" $fileout
-wl-copy < $fileout
+[ -d "$screenshot_dir" ]; grim -g "$(slurp)" $fileout | wl-copy
